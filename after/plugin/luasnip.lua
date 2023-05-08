f113ce136e542cdddd1855131f8c915652d4ae77
Patricio Urrutia <purrutia@gmail.com>
2023-05-07 21:47:58 -0400

    Change folder name LuaSnip to LuaSnippets
    Change folder name for LuaSnip snippets from LuaSnip to LuaSnippets.
    The main reason is because LuaSnippets is going to live on his own
    repo, so, the name LuaSnip could be a little confusing.

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,2 +1,2 @@
 -- Lazy Load snippets, ie only load when required
-require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnip/snippets/" })
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnippets/snippets/" })
