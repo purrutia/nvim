e8993b1cabae43a5586049581dfd3a077c822b26
Patricio Urrutia <purrutia@gmail.com>
2023-05-22 22:42:44 -0400

    Changed Luasnip folder to .local/nvim/LuaSnippets/snippets

--- after/plugin/luasnip.lua
+++ after/plugin/luasnip.lua
@@ -1,5 +1,5 @@
 -- Lazy Load snippets, ie only load when required
-require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnippets/snippets/" })
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.local/nvim/LuaSnippets/snippets/" })
 -- require("luasnip").config.set_config({
 --   enable_autosnippets = true,
 -- })
