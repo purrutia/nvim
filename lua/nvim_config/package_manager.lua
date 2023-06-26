a572b77ad5c4753776acafe5ba3dbd9ea8d454eb
Patricio Urrutia <purrutia@gmail.com>
2023-06-25 23:13:43 -0400

    Deleted pandoc and pandoc syntax

--- lua/nvim_config/package_manager.lua
+++ lua/nvim_config/package_manager.lua
@@ -180,9 +180,6 @@ require('lazy').setup({
     end
   },
 
-  -- Pandoc and markdown
-  'vim-pandoc/vim-pandoc',
-  'rwxrob/vim-pandoc-syntax-simple',
 
   -- NOTE: Next Step on your Neovim journey: Add/Configure additional plugins
 
