6e7b41e16ce2c8402af9240c5f70245a12db00ae
Patricio Urrutia <purrutia@gmail.com>
2023-11-01 17:48:23 -0300

    update keymaps on nvimtree

--- lua/purrutia/plugins/nvimtree.lua
+++ lua/purrutia/plugins/nvimtree.lua
@@ -9,5 +9,7 @@ return {
     require("nvim-tree").setup()
 
     vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+    vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" })
+    vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse File Explorer" })
   end
 }
