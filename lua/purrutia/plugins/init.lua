9fe56ed84ec96e939b699216aebb4e265d5a9201
Patricio Urrutia <purrutia@gmail.com>
2023-11-01 22:22:24 -0300

    All telescope config on plugins folder and keymaps for fugitive

--- lua/purrutia/plugins/init.lua
+++ lua/purrutia/plugins/init.lua
@@ -26,8 +26,6 @@ return {
     dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip', 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline' },
   },
 
-  -- Useful plugin to show you pending keybindings.
-  { 'folke/which-key.nvim', opts = {} },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
--- lua/purrutia/plugins/tpope.lua
+++ lua/purrutia/plugins/tpope.lua
@@ -1,6 +1,12 @@
 return {
   -- [[ Git Related plugins ]]
-  'tpope/vim-fugitive',
+  {
+    'tpope/vim-fugitive',
+    config = function()
+        vim.keymap.set("n", "<leader>g", "<cmd>G<CR>", { desc = "Git project manage" })
+        vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git push" })
+    end
+  },
   'tpope/vim-rhubarb',
 
   -- Detect tabstop and shiftwidth automatically
