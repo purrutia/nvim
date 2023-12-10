106792275fe009a9d9fec08ba61ac3c79587f557
Patricio Urrutia <purrutia@gmail.com>
2023-12-09 22:28:35 -0300

    nvimtree config indentation fixed

--- lua/purrutia/plugins/nvimtree.lua
+++ lua/purrutia/plugins/nvimtree.lua
@@ -1,15 +1,15 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- disable netrw
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		-- disable netrw
+		vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
 
-    require("nvim-tree").setup()
+		require("nvim-tree").setup()
 
-    vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
-    vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" })
-    vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse File Explorer" })
-  end
+		vim.keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle File Explorer" })
+		vim.keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh File Explorer" })
+		vim.keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse File Explorer" })
+	end,
 }
