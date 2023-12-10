345853df89a0055de15d5f8a1bae0a824a7be271
Patricio Urrutia <purrutia@gmail.com>
2023-12-09 22:29:33 -0300

    vimwiki config fixed! - Use init instead of config if you need the config loaded before the   plugin - Different colors for title levels - comment style when the task is completed

--- lua/purrutia/plugins/vimwiki.lua
+++ lua/purrutia/plugins/vimwiki.lua
@@ -1,15 +1,19 @@
 -- [[ Vimwiki ]]
 return {
-  'vimwiki/vimwiki',
-  -- config = function()
-  --   vim.g.vimwiki_global_ext = 0
-  --   vim.g.vimwiki_list = {
-  --     {
-  --       auto_export = 1,
-  --       path = '~/vimwiki/',
-  --       syntax = 'markdown',
-  --       ext = '.md',
-  --     }
-  --   }
-  -- end
+	"vimwiki/vimwiki",
+	-- Options for vimwiki needs to be set before the plugin loads
+	init = function()
+		vim.g.vimwiki_global_ext = 0
+		vim.g.vimwiki_hl_headers = 1
+		vim.g.vimwiki_hl_cb_checked = 1
+		-- list of wikis per folder, this case just one at ~/vimwiki and those are markdown by default
+		vim.g.vimwiki_list = {
+			{
+				path = "~/vimwiki/",
+				syntax = "markdown",
+				ext = ".md",
+			},
+		}
+		-- files are not added automatically to a wiki when are outside the vimwiki_list
+	end,
 }
