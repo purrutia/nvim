413181219700a20ad050b2e727676b98c442560f
Patricio Urrutia <purrutia@gmail.com>
2023-12-29 17:38:29 -0300

    Trying nvim-surround instead of tpope surround

--- lua/purrutia/plugins/tpope.lua
+++ lua/purrutia/plugins/tpope.lua
@@ -1,17 +1,17 @@
 return {
-  -- [[ Git Related plugins ]]
-  {
-    'tpope/vim-fugitive',
-    config = function()
-        vim.keymap.set("n", "<leader>g", "<cmd>G<CR>", { desc = "Git project manage" })
-        vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git push" })
-    end
-  },
-  'tpope/vim-rhubarb',
+	-- [[ Git Related plugins ]]
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>g", "<cmd>G<CR>", { desc = "Git project manage" })
+			vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git push" })
+		end,
+	},
+	"tpope/vim-rhubarb",
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+	-- Detect tabstop and shiftwidth automatically
+	"tpope/vim-sleuth",
 
-  -- Change Surroundings
-  'tpope/vim-surround',
+	-- Change Surroundings
+	-- 'tpope/vim-surround',
 }
