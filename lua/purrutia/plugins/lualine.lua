9e96a895123cd60e0d1b5661e2dcef73a7491794
Patricio Urrutia <purrutia@gmail.com>
2024-01-12 21:27:33 -0300

    Noice problem with macros fixed in lualine

--- lua/purrutia/plugins/lualine.lua
+++ lua/purrutia/plugins/lualine.lua
@@ -1,13 +1,38 @@
 -- lualine
 return { -- set lualine as statusline
-  'nvim-lualine/lualine.nvim',
-  -- See `:help lualine.txt`
-  opts = {
-    options = {
-      icons_enabled = false,
-      theme = 'palenight',
-      component_separators = '|',
-      section_separators = '',
-    },
-  },
+	"nvim-lualine/lualine.nvim",
+	-- See `:help lualine.txt`
+	config = function()
+		require("lualine").setup({
+			options = {
+				icons_enabled = false,
+				theme = "palenight",
+				component_separators = "|",
+				section_separators = "",
+			},
+			sections = {
+				lualine_x = {
+					{
+						require("noice").api.status.message.get_hl,
+						cond = require("noice").api.status.message.has,
+					},
+					{
+						require("noice").api.status.command.get,
+						cond = require("noice").api.status.command.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						require("noice").api.status.mode.get,
+						cond = require("noice").api.status.mode.has,
+						color = { fg = "#ff9e64" },
+					},
+					{
+						require("noice").api.status.search.get,
+						cond = require("noice").api.status.search.has,
+						color = { fg = "#ff9e64" },
+					},
+				},
+			},
+		})
+	end,
 }
