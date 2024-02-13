da31669e9b35cc43eb3be23f35c8d94694e98449
Patricio Urrutia <purrutia@gmail.com>
2024-02-13 16:10:19 -0300

    Turn off Twilight on ZenMode by default

--- lua/purrutia/plugins/zen-mode.lua
+++ lua/purrutia/plugins/zen-mode.lua
@@ -1,9 +1,12 @@
 -- [[ Distraction Free ]]
 return {
-  "folke/zen-mode.nvim",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+	"folke/zen-mode.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		plugins = {
+			twilight = { enabled = false },
+		},
+	},
 }
