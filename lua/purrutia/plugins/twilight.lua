5ebf8b3f83cd34b5396116d357a9761af41edb14
Patricio Urrutia <purrutia@gmail.com>
2024-02-12 22:15:24 -0300

    Added twilight for zen mode... Folkeeeee

---
+++ lua/purrutia/plugins/twilight.lua
@@ -0,0 +1,9 @@
+-- [[ Distraction Free Twilight ]]
+return {
+	"folke/twilight.nvim",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+}
