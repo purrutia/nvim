0e06463d5d1cd67a1af6e680d7f8a9e0b89237d5
Patricio Urrutia <purrutia@gmail.com>
2024-02-14 11:08:33 -0300

    Zenmode: disable twilight, hide tmux and width set to 80%

--- lua/purrutia/plugins/zen-mode.lua
+++ lua/purrutia/plugins/zen-mode.lua
@@ -5,8 +5,12 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		window = {
+			width = 0.8,
+		},
 		plugins = {
 			twilight = { enabled = false },
+			tmux = { enabled = true },
 		},
 	},
 }
