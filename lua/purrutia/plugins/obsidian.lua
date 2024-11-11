689ab41c911ae7692fb9caa6648efd04263941b0
Patricio Urrutia <purrutia@gmail.com>
2024-11-11 14:52:23 -0300

    Add title to the frontmatter of the file, to use titles on graphview in obsidian

--- lua/purrutia/plugins/obsidian.lua
+++ lua/purrutia/plugins/obsidian.lua
@@ -41,6 +41,25 @@ return {
 				template = "Daily_Note_template.md",
 			},
 
+			note_frontmatter_func = function(note)
+				-- Add the title of the note as an alias
+				if note.title then
+					note:add_alias(note.title)
+				end
+
+				local out = { id = note.id, aliases = note.aliases, tags = note.tags, title = note.title }
+
+				-- `note.metadata` contains any manually added fields in the frontmatter.
+				-- So here we just make sure those fields are kept in the frontmatter.
+				if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+					for k, v in pairs(note.metadata) do
+						out[k] = v
+					end
+				end
+
+				return out
+			end,
+
 			-- see below for full list of options 👇
 			-- -- A list of vault names and paths.
 			-- -- Each path should be the path to the vault root. If you use the Obsidian app,
