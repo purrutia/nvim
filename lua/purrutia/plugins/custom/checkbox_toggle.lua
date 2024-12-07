d948b58c839d648ca9c36e7414820c99d57c1aa5
purrutia <purrutia@gmail.com>
2024-12-07 07:50:43 -0300

    Change /home/purrutia for ~ in checkbox_toggle

--- lua/purrutia/plugins/custom/checkbox_toggle.lua
+++ lua/purrutia/plugins/custom/checkbox_toggle.lua
@@ -1,6 +1,6 @@
 -- config for checkbox_toggle custom plugin
 return {
-	dir = "$HOME/Documents/1_Projects/checkbox_toggle.nvim/",
+	dir = "~/Documents/1_Projects/checkbox_toggle.nvim/",
 	config = function()
 		require("checkbox_toggle").setup()
 	end,
